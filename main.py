1ff574d16cabd2057edd88c0272fc9d9a1ca7659
Katharina <katkra1994@googlemail.com>
2021-04-27 14:33:37 +0200

    Some comments and explanations

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ import pandas as pd
 import requests
 import argparse
 from sqlalchemy import create_engine
-import sys
-import sqlite3 as lite
-import sys
-from pandas.io import sql
-from pandas import DataFrame
 
 
+#ToDo Error handling user input
 # Create the parser
 my_parser = argparse.ArgumentParser(prog='CLI',description='Organization information from GitHub')
 
@@ -21,22 +17,18 @@ my_parser.add_argument('Organization',
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
-
+#User input argument
 input_organization = args.Organization
 
-
-
-headers={"Authorization": "bearer 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"}
+headers={"Authorization": "bearer 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"}
 
 url = 'https://api.github.com/graphql'
 #ToDo: could read in all data about Organizations but later create database entries only with few selected entries
-
-#ToDo exchange specific organization with variables-values will be set by user input
 #ToDo Pagination-get all repositories and members (not just first 5)
 #ToDo CommitConnection(Commit)database-sha
 
-#variables={"Orga":sys.argv[1]}
 variables={"Orga":input_organization}
+#Some relevant organization data is queried via the GraphQL GitHub API
 query_exOrganization="""
 query{
   organization(login: "%(Orga)s"){
@@ -65,64 +57,41 @@ query{
     }
     }
   }"""%(variables)
-#variables={"Orga":'github'}
+#ToDo Error handling for None values in any query related output(maybe default substitute)-> sql does not like it
+
 #ToDo create Repository database with more detailed data-post request,save in file,normalize then create database
-query_exRepo = """"
-query {
-  repository(owner:"github",name:"media"){
-    name
-    url
-    id
-    isPrivate
-    createdAt
-    updatedAt
-    
-    
-    }
-  }"""
 
-
+#the "answer" from the API
 r = (requests.post(url, json={'query': query_exOrganization},headers=headers))
-
-r_json = r.json()
+#is transformed to json and written into a .json file
+json_data = json.loads(r.text)
 print(r_json)
 jsonString = json.dumps(r_json)
-jsonFile = open("Github_data.json", "w")
+jsonFile = open("%(Orga)s_data.json", "w")
 jsonFile.write(jsonString)
 jsonFile.close()
-df_data = pd.read_json("Github_data.json", orient='columns')
-df = pd.DataFrame(df_data)
-
-
-json_data = json.loads(r.text)
-
+#reading json into several dataframes-members and repositories have lists that need to specially be normalized
 df_organization=json_data['data']['organization']
-
-
 
 df_repo = json_data['data']['organization']['repositories']
 df_members=json_data['data']['organization']['membersWithRole']
-
-
+#we only want some information in our database- .loc
 dfOrganization=pd.json_normalize(df_organization)
-
-
 df_Organization=dfOrganization.loc[:,['id','name','url','description']]
-
 
 dfMembers = pd.json_normalize(df_members,record_path=['nodes'])
 dfMembers=dfMembers.loc[:,['id']]
+#we want to create a relation database that represents the connection between the organisation and the members, so the organization ID is added for all members
 dfMembers['organization_id']=df_Organization['id'][0]
+
 dfRepo=pd.json_normalize(df_repo,record_path=['nodes'])
+#the relation of the repositories and organizations also requires the organization ID
 dfRepo['organization_id'] = df_Organization['id'][0]
 
-
-
-
+#ToDo one could think about adding rows in all databases (if they exist locally) for each new organization and related queries
+#lastly we create the databases and save the into a file
 engine = create_engine('sqlite:///save_pandas.db', echo=True)
 sqlite_connection = engine.connect()
 dfMembers.to_sql("MembersOrganizationConnection",sqlite_connection,if_exists='fail')
 dfRepo.to_sql("RepositoryOrganizationConnection", sqlite_connection, if_exists='fail')
 df_Organization.to_sql("Organizations",sqlite_connection,if_exists='fail')
-
-#ToDo one could think about adding rows in all databases (if they exist locally) for each new organization and related queries
